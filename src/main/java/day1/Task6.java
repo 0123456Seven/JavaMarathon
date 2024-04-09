c02fce0ba89e00ac2957bc04d588b35c0847340e
Bagautdinov Marsel <mefefef@bk.ru>
2024-04-09 14:59:13 +0500

    add JavaMarathon project

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 8;
+        for(int i = 0; i<=k;i++){
+            System.out.println(i+" x "+k + " = "+ i*k);
+        }
+    }
+}
