b0f7b5fb5b78c45633730f63bd6a1e62a3d5986a
Bagautdinov Marsel <mefefef@bk.ru>
2024-04-09 15:56:43 +0500

    add day2 and day3_1

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a>b||a==b){
+            System.out.println("ERROR");
+        }
+        else{
+            while(a!=b){
+                if(a%5==0&&a%10>0){
+                    System.out.println(a);
+                }
+                a++;
+            }
+        }
 
     }
 }
--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -4,6 +4,56 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        final String RUSSIA = "Россия";
+        final String ITALY = "Италия";
+        final String ENGLAND = "Англия";
+        final String GERMAN = "Германия";
+        Scanner scanner = new Scanner(System.in);
+        String city = scanner.nextLine();
+        while(!city.equals("Stop")){
+            switch (city){
+                case "Москва":
+                    System.out.println(RUSSIA);
+                    break;
+                case "Владивосток":
+                    System.out.println(RUSSIA);
+                    break;
+                case "Ростов":
+                    System.out.println(RUSSIA);
+                    break;
+                case "Рим":
+                    System.out.println(ITALY);
+                    break;
+                case "Турин":
+                    System.out.println(ITALY);
+                    break;
+                case "Милан":
+                    System.out.println(ITALY);
+                    break;
+                case "Ливерпуль":
+                    System.out.println(ENGLAND);
+                    break;
+                case "Манчерстер":
+                    System.out.println(ENGLAND);
+                    break;
+                case "Лондон":
+                    System.out.println(ENGLAND);
+                    break;
+                case "Мюнхен":
+                    System.out.println(GERMAN);
+                    break;
+                case "Берлин":
+                    System.out.println(GERMAN);
+                    break;
+                case "Кельн":
+                    System.out.println(GERMAN);
+                    break;
+                default:
+                    System.out.println("Неизветстная страна");
+                    break;
 
+            }
+            city = scanner.nextLine();
+        }
     }
 }
